e8a321fbc50fa8e07bc95a0b141ea338dd18031c
Mark Chang <mark.chang@vpon.com>
2024-04-21 20:30:14 +0800

    leetcode 141

---
+++ linked_list/141.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/problems/linked-list-cycle/submissions/
+"""
+from typing import Optional
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None or head.next is None:
+            return False
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow is fast:
+                return True
+        return False
