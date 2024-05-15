2e4f6b35c4f351a96e292d7b74dcf3604b2b5b7c
Mark Chang <mark.chang@vpon.com>
2024-05-15 21:59:32 +0800

    add leetcode 100 bfs method

--- trees/100.py
+++ trees/100.py
@@ -1,5 +1,6 @@
 """https://leetcode.com/problems/same-tree/."""
 from typing import Optional
+from collections import deque
 
 
 # Definition for a binary tree node.
@@ -10,7 +11,7 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
+class DFSSolution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         s = []
         s.append((p, q))
@@ -25,4 +26,22 @@ class Solution:
                     return False
                 s.append((p.right, q.right))
                 s.append((p.left, q.left))
+        return True
+
+
+class BFSSolution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        queue = deque()
+        queue.append((p, q))
+        while queue:
+            p, q = queue.popleft()
+            if p is None and q is not None:
+                return False
+            if q is None and p is not None:
+                return False
+            if p is not None or q is not None:
+                if p.val != q.val:
+                    return False
+                queue.append((p.left, q.left))
+                queue.append((p.right, q.right))
         return True
