90ac6a97db55fe584e8d17f5622d12cdfbe13be2
Mark Chang <mark.chang@vpon.com>
2024-09-28 22:24:15 +0800

    add leetcode 1045

---
+++ sql/1045.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/customers-who-bought-all-products/
+select
+customer_id
+from Customer
+group by customer_id
+having count(distinct product_key) = (select count(product_key) from Product)
