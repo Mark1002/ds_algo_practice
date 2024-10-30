9221f1f07241c6b99833aa568c47a475685974bc
Mark Chang <mark.chang@vpon.com>
2024-10-30 21:19:30 +0800

    add leetcode 1667

---
+++ sql/1667.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/fix-names-in-a-table/
+SELECT
+    user_id,
+    CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) as name
+from Users
+ORDER BY user_id
