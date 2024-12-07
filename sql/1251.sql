951e88c978a9e3116763afc2cbc5158d5ea084fb
Mark Chang <mark.chang@vpon.com>
2024-12-07 21:15:36 +0800

    add leetcode 1251

---
+++ sql/1251.sql
@@ -0,0 +1,11 @@
+-- https://leetcode.com/problems/average-selling-price
+select
+    p.product_id,
+    coalesce(round(sum(u.units * p.price)*1.0/sum(u.units), 2), 0) as average_price
+from Prices p
+left join UnitsSold u
+on (p.product_id = u.product_id
+and u.purchase_date 
+between p.start_date and p.end_date
+)
+group by p.product_id
