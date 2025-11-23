e1d9be92db58f8487dac004d16d32bc5d94e334b
Mark Chang <ak0922747401@gmail.com>
2025-11-23 14:04:53 +0800

    add bfs solution for leetcode 200

--- graph/200.py
+++ graph/200.py
@@ -1,9 +1,9 @@
 """https://leetcode.com/problems/number-of-islands/"""
 from typing import List
-
+from collections import deque
 
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands_dfs(self, grid: List[List[str]]) -> int:
         row, col = len(grid), len(grid[0])
         count = 0
 
@@ -21,4 +21,34 @@ class Solution:
                     continue
                 dfs(i, j)
                 count += 1
+        return count
+    
+    def numIslands_bfs(self, grid: List[List[str]]) -> int:
+        row, col = len(grid), len(grid[0])
+        count = 0
+    
+        def bfs(r: int, c: int):
+            direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            rows, cols = len(grid), len(grid[0])
+            queue = deque()
+            queue.append((r, c))
+            # make as visited
+            grid[r][c] = "0"
+
+            while queue:
+                r, c = queue.popleft()
+                for dr, dc in direction:
+                    nr, nc = r+dr, c+dc
+                    if nr >= 0 and nr < rows and nc >= 0 and nc < cols and grid[nr][nc] == "1":
+                        queue.append((nr, nc))
+                        # make as visited 
+                        grid[nr][nc] = "0"
+
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == "0":
+                    continue
+                bfs(i, j)
+                # finish traversal means find a island
+                count+=1
         return count
